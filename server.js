7c07ec6f28468e631a18da8305f2b06e846c1df8
Alex Illarionov <illarionoff.a@gmail.com>
2019-01-29 20:59:27 +0900

    MongoDB config, Mongoose connection,  Routes added

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
+// Routes
+const users = require("./routes/api/users");
+const movies = require("./routes/api/blogposts");
+
+// Initialize express app
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// User Routes
+app.use("/api/users", users);
+
+app.use("/api/movies", movies);
 
 // Server PORT
 const port = process.env.PORT || 5000;
