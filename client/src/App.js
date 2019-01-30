646d78baf3675bff2b0621f89e5e51ab662737a9
Alex Illarionov <illarionoff.a@gmail.com>
2019-01-30 17:53:24 +0900

    React routes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Login from "./components/Login";
+import Main from "./components/Main";
+import My404Component from "./components/My404Component";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/login" component={Login} />
+            <Route path="*" component={My404Component} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
